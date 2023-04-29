a7a919bd06cfff78a9947c01a9d48e2c7af65171
Alex Knowles <git@alexknowles.info>
2023-04-29 12:37:37 +0100

    Add: Temp scene stuff

---
+++ Assets/Scripts/Packages/PackageData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HaveYouGotAMoment.Package
+{
+    public class PackageData : MonoBehaviour
+    {
+        public string Courier;
+		public string Tenant;
+	}
+}
