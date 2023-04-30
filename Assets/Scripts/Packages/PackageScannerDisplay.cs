49754acd6b6a9eac44611ffb92cd7431f90605eb
Lee Martin <hello@leejohnmartin.co.uk>
2023-04-30 13:00:31 +0100

    Fix: Remove duplicate Manager

--- Assets/Scripts/Packages/PackageScannerDisplay.cs
+++ Assets/Scripts/Packages/PackageScannerDisplay.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 
@@ -15,13 +16,20 @@ namespace HaveYouGotAMoment
 
         private string _text;
         private GameObject _package;
-        private GameObject _tenantPrefab;
+        private GameObject _displayTenant;
+
+        private TenantManager _tenantManager;
 
         // Start is called before the first frame update
         void Start()
         {
             _displayText = DisplayText.GetComponent<TextMeshPro>();
             _text = DefaultText;
+
+            if (_tenantManager == null)
+            {
+                _tenantManager = GameObject.FindGameObjectWithTag("Managers").GetComponent<TenantManager>();
+            }
         }
 
         // Update is called once per frame
@@ -40,14 +48,27 @@ namespace HaveYouGotAMoment
         {
             if (_package != null)
             {
+                var tenantName = _package.GetComponent<Packages.PackageData>().Tenant;
                 // Get name
-                _text = _package.GetComponent<Packages.PackageData>().Tenant;
+                _text = tenantName;
                 // Get tenant prefab
+                var tenant = _tenantManager.PossibleTenants.First(x => x.GetComponent<Tenants.TenantData>().TenantName == tenantName);
+                var displayTenant = Instantiate(tenant, transform);
+                Destroy(displayTenant.GetComponent<Scheduler>());
+                Destroy(displayTenant.GetComponent<Tenants.TenantDialog>());
+                Destroy(displayTenant.GetComponent<Tenants.TenantClick>());
+                Destroy(displayTenant.GetComponent<Tenants.TenantMovement>());
+                Destroy(displayTenant.GetComponent<TenantPackageHandler>());
+                _displayTenant = displayTenant;
             }
             else
             {
                 _text = DefaultText;
-                _tenantPrefab = null;
+                if (_displayTenant != null)
+                {
+                    Destroy(_displayTenant);
+                    _displayTenant = null;
+                }
             }
         }
     }
